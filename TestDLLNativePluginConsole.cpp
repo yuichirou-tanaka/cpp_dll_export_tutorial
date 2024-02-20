5ec5d8cbb33644e1fab153f822e70a337941a573
YuichiroTanaka/田中雄一郎 <80798265+yuichirou-tanaka@users.noreply.github.com>
2024-02-20 11:25:25 +0900

    Add files via upload

---
+++ TestDLLNativePluginConsole.cpp
@@ -0,0 +1,26 @@
+﻿// TestDLLNativePluginConsole.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+
+//#ifdef __WINDOWS
+    #define DLLEXPORT __declspec(dllexport)
+//#else
+    //#define DLLEXPORT 
+//#endif
+
+extern "C" {
+    DLLEXPORT int IntTest(int x) {
+        x *= 2;
+        return x;
+    }
+}
+
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+    int id =   IntTest(2);
+    std::cout << id << std::endl;
+}
